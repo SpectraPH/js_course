d8f6bf39ecbcfabbde9e250207a335a6cf55f4c4
Petr Zhykharskyi <petr220120017@gmail.com>
2022-09-09 22:46:19 +0300

    add string basic tasks

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function stringToNumbersArray(str){
+   
+}
+
+function countWordsInText(word, text){
+  
+}
+
+function countLetterInText(letter, text){
+  
+}
+
+function removeWordFromText(word, text){
+   
+}
+
+export {stringToNumbersArray, countWordsInText, countLetterInText, removeWordFromText};
