2987fafe01b1817498ba84dfd0f3a24e2886d06b
Petr Zhykharskyi <petr220120017@gmail.com>
2022-09-13 22:45:35 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 function arrayToString(array){
-
+    return [];
 }
 
 function max(array){
-
+    return [];
 }
 
 function indexOfMax(array){
-
+    return [];
 }
 
 function sliceArray(array, n){
-
+    return [];
 }
 
 function sort(array){
-
+    return [];
 }
 
 function sumArrays(array1, array2){
-
+    return [];
 }
 
 function removeDuplicates(array){
-
+    return [];
 }
 
 export {arrayToString, max, indexOfMax, removeDuplicates, sliceArray, sort, sumArrays}
